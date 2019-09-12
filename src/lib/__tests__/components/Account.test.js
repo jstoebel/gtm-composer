7359b871fd6a2cc69d7a10de53f741bda8593650
Jacob Stoebel <jstoebel@gmail.com>
2019-09-12 16:52:29 -0400

    test for updating account name

--- src/lib/__tests__/components/Account.test.js
+++ src/lib/__tests__/components/Account.test.js
@@ -3,7 +3,7 @@ import {render} from 'ink-testing-library'
 import wait from 'waait'
 
 const getAccountWithContext = (client) => {
-  jest.doMock('../clientContext', () => {
+  jest.doMock('../../clientContext', () => {
     return {
       clientContext: {
         Consumer: (props) => props.children(client)
@@ -15,99 +15,47 @@ const getAccountWithContext = (client) => {
 }
 
 describe('Account', () => {
-  let mockContainerList;
-  let wrapper;
-  let mockChildren = jest.fn();
-  const resultContainers = jest.fn()
-  const mockList = jest.fn(() => {
-    return new Promise((resolve) => {
-      resolve({
-        data: {
-          container: resultContainers
+  describe('updates account name', () => {
+    test('updates account from accountId', () => {
+      let mockContainerList;
+      let wrapper;
+      let mockChildren = jest.fn();
+      const resultContainers = jest.fn()
+      const mockList = jest.fn(() => {
+        return new Promise((resolve) => {
+          resolve({
+            data: {
+              container: resultContainers
+            }
+          })
+        })
+      });
+    
+      const mockUpdate = jest.fn(() => {
+        return new Promise((resolve) => { resolve() })
+      });
+      const mockClient = {
+        accounts: {
+          containers: {
+            list: mockList
+          },
+          update: mockUpdate
         }
-      })
-    })
-  });
-
-  const mockUpdate = jest.fn(() => {
-    return new Promise((resolve) => { resolve() })
-  });
-
-  const mockClient = {
-    accounts: {
-      containers: {
-        list: mockList
-      },
-      update: mockUpdate
-    }
-  }
-
-  const Account = getAccountWithContext(mockClient)
-
-  describe('fetches containers by accountId', () => {
-    const accountId = 1
-    beforeEach(() => {
-
+      }
+    
+      const Account = getAccountWithContext(mockClient)
+      const accountId = 1;
+      const newName = 'new name'
       wrapper = render(
         <Account accountId={accountId}>
           {mockChildren}
         </Account>
       )
-    })
-    test('fetches all containers in account', async () => {
-      await wait(0);
-      expect(mockList).toHaveBeenLastCalledWith({ parent: `accounts/${accountId}`});
-    })
 
-    test('exposes containers to render props function', async () => {
-      await wait(0);
-      expect(mockChildren).toHaveBeenLastCalledWith(resultContainers)
+      expect(mockUpdate).toHaveBeenLastCalledWith({newName})
     })
+  
   })
 
-  describe('updates account name', () => {
-    beforeEach(() => {
-
-    })
-    it('updates account name', async () => {
-      wrapper = render(
-        <Account name= {'Old Name'} newName={'New Name'}>
-          {mockChildren}
-        </Account>
-      )
-      await wait(0);
-      expect(mockUpdate).toHaveBeenLastCalledWith({requestBody: {name: 'New Name'}});
-    })
-    
-    it('leaves account name unchanged', async () => {
-      wrapper = render(
-        <Account name={'Old Name'} newName={'Old Name'}>
-          {mockChildren}
-        </Account>
-      )
-      await wait(0);
-      expect(mockUpdate).not.toHaveBeenCalled();
-    })
-
-    it('leaves name unchanged when no name given', async () => {
-      wrapper = render(
-        <Account name={'Old Name'}>
-          {mockChildren}
-        </Account>
-      )
-      await wait(0);
-      expect(mockUpdate).not.toHaveBeenCalled()
-    })
-
-    it('leaves name unchanged when no newName given', async () => {
-      wrapper = render(
-        <Account newName={'New Name'}>
-          {mockChildren}
-        </Account>
-      )
-      await wait(0);
-      expect(mockUpdate).not.toHaveBeenCalled()
-    })
-
-  })
+  test.todo('exposes containers to render prop function')
 })
